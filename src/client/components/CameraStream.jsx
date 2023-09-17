e07f70ffbd0ff25e152878f0854a9b11a064bb20
quochuy2405 <huy01682312920@gmail.com>
2023-09-17 10:10:00 +0700

    camera-with-object

---
+++ src/client/components/CameraStream.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./CameraStream.css";
+const CameraStream = () => {
+	return (
+		<div id="player-container">
+			<video id="player-video" autoplay playsinline></video>
+		</div>
+	);
+};
+
+export default CameraStream;
