2fe7de20a2979fd8f8a185dfca7a7987da4442ba
Minh Khuy <haokhuy@gmail.com>
2023-09-29 11:30:08 +0700

    correct event

--- src/client/drawCanvas.js
+++ src/client/drawCanvas.js
@@ -24,7 +24,7 @@ let playersNameMap = {};
 
 var mouseCoorX = 0;
 var mouseCoorY = 0;
-var initFunction = false
+var initFunction = false;
 var showNames = false;
 
 var directionCoors = [
@@ -46,7 +46,6 @@ export function drawInit() {
     mouseCoorX = e.clientX - canvas.getBoundingClientRect().x;
     mouseCoorY = e.clientY - canvas.getBoundingClientRect().y;
   };
-
 }
 
 //coordinates to draw bar indicating location of player offscreen
@@ -122,8 +121,11 @@ function draw(x, y, map, players) {
 
   ctx.drawImage(terrainImages[map], top_x, top_y, w, h, 0, 0, w, h);
   if (!initFunction) {
-    initFunction = true
-    listenerIdObject(map, top_x, top_y, objectSizes)
+    setTimeout(() => {
+      initFunction = false;
+    }, 3000);
+    initFunction = true;
+    listenerIdObject(map, top_x, top_y, objectSizes);
   }
   // ctx.beginPath();
   // ctx.lineWidth = "4";
